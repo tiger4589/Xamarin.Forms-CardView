58b0b5a75e878c17e800bfa5a648e0f38fa44044
Paul <paulkaram1989@hotmail.com>
2017-03-15 00:28:12 +0200

    updating package info

--- CardView/Properties/AssemblyInfo.cs
+++ CardView/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
